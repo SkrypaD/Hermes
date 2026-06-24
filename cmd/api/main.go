eda97050ddde6884ac9b1ea82043c0426adc5423
SkrypaD <skrypchenkodanil@gmail.com>
2026-06-24 11:42:34 +0000

    Initial setup

---
+++ cmd/api/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	PORT := os.Getenv("PORT")
+
+	g := gin.Default()
+
+	g.Run(PORT)
+}
